54c27b73bdd1c3ea15bb1cdbc7c3c33340cee6d3
Daniel Santos <danieldsm.on@gmail.com>
2024-09-28 01:02:32 -0300

    fix validate option function to show correct message

--- option.py
+++ option.py
@@ -2,8 +2,8 @@ def validate_option(option):
     option = option.lower()
 
     while option != "s" and option != "n":
-        print("Inválido!")
-        option = input("(S - sim) ou (N - não): ")
+        print("\nInválido!")
+        option = input("S - sim\nN - não\nDigite novamente: ")
         option = option.lower()
 
     return option
